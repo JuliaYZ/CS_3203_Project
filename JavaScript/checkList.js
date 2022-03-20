b4b2857b35b74b9100c11a69ea593f2e6f253fc9
Yan Zhou <yanzhou0311@gmail.com>
2022-03-19 23:56:21 -0500

    Add Stick Notes and Calculator, error occur

---
+++ JavaScript/checkList.js
@@ -0,0 +1,13 @@
+function myCheckList() {
+    var x = document.getElementById("checkList");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function xcl() {
+    var x = document.getElementById("checkList");
+    x.style.display = "none";
+}
