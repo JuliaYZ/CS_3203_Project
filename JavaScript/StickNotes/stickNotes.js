49bcafc195423c9f2fdded177dd7c8c720400385
Shiyuan Liu <Shiyuan.Liu-1@ou.edu>
2022-04-07 16:11:06 -0500

    move to status

--- JavaScript/StickNotes/stickNotes.js
+++
@@ -1,6 +0,0 @@
-// <cite><a href="https://github.com/dcode-youtube/notes-app-javascript-localstorage"></a></cite>
-
-import App from "./App.js";
-
-const root = document.getElementById("app");
-const app = new App(root);
